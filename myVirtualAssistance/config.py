dc4d496cfcb7af1f9da73daecf8174905d4b9a15
DipeshMate <78660765+DipeshMate@users.noreply.github.com>
2024-01-19 13:43:31 +0530

    Update config.py

--- myVirtualAssistance/config.py
+++ myVirtualAssistance/config.py
@@ -1,6 +1,6 @@
 # todo: add your api here
-apikey = "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"
+apikey = "sk-5yIe4KrGSz8Pj9pCYxvnT3BlbkFJAzLMLJfB**********"
 
-# apikey = "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"
+# apikey = "sk-KzxOFKhD8vSqnP0pt5MBT3BlbkFJI5oJqsN0My**********"
 
-# apikey = "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"
+# apikey = "sk-XkveccFXYAQg6AAOK6uYT3BlbkFJpfg2DUZpHvqnqP8t****"
